cb3b0748bbed899db341242b1b74e54ae0e03e9c
Levi Tuoremaa <ltuoremaa@gmail.com>
2022-10-25 21:31:57 +0200

    Implementerade memory leaks

--- Tracer2/Tracer.cpp
+++ Tracer2/Tracer.cpp
@@ -38,13 +38,52 @@ Color trace_ray(Scene myscene, const Ray& r, int depth) {
 
 Color path_tracer(Scene myscene, Ray& r) {
 	// Build a ray path and save the final ray
+	//std::cout << "\nRay sent from " << r.get_origin();
+
 	Ray last_ray = build_path(myscene, r);
+	//std::cout << "\nDepth of the last ray is " << last_ray.depth;
+
+	//std::cout << "\nPrinting raypath:";
+	//print_raypath(r);
+	//print_reverse_raypath(last_ray);
 
 	return terminate_ray(myscene, last_ray);
 }
 
+void print_raypath(Ray& first_ray) {
+	Ray* r = &first_ray;
+	int i = 1;
+	while (true) {
+		std::cout << "\nDepth of ray " << i << ": " << r->depth;
+		i++;
+		r = r->next_ray;
+		if (r == nullptr) {
+			std::cout << "\nr was null at ray " << i;
+			break;
+		}
+	}
+}
+
+void print_reverse_raypath(Ray& last_ray) {
+	Ray* r = &last_ray;
+	int i = last_ray.depth + 1;
+	while (true) {
+		std::cout << "\nDepth of ray " << i << ": " << r->depth;
+		i--;
+		r = r->prev_ray;
+		if (r == nullptr) break;
+	}
+}
+
 Ray build_path(Scene myscene, Ray& origin_ray) {
-	if (origin_ray.depth > 20) {
+	//std::cout << "\nProcessing ray of depth " << origin_ray.depth;
+	if (origin_ray.prev_ray == nullptr) {
+		//std::cout << "\nPrevious ray is null";
+	}
+	else {
+		//std::cout << "\nPrevious ray has depth " << origin_ray.prev_ray->depth;
+	}
+	if (origin_ray.depth >= 7) {
 		// sanity check
 		return origin_ray;
 	}
@@ -53,6 +92,7 @@ Ray build_path(Scene myscene, Ray& origin_ray) {
 	hit_record nearest_rec; // to store the hit record specifically for the nearest object hit
 	double lowest_t = 10000;
 	Shape* nearest_object = nullptr;
+	Ray* ref_ray;
 
 	// Check every shape in myscene for a collision
 	for (int i = 0; i < myscene.get_size(); i++) {
@@ -61,6 +101,7 @@ Ray build_path(Scene myscene, Ray& origin_ray) {
 			if (rec.t < lowest_t && rec.t > 0.001) {
 				// Update nearest object
 				nearest_rec = rec;
+				lowest_t = rec.t;
 				nearest_object = myscene[i];
 			}
 		}
@@ -68,6 +109,9 @@ Ray build_path(Scene myscene, Ray& origin_ray) {
 
 	if (nearest_object != nullptr) {
 		// There was a collision
+		//std::cout << "\nCollision occured at: ";
+		//std::cout << origin_ray.at(nearest_rec.t);
+
 		if (nearest_object->get_material().light()) {
 			// terminate path
 			origin_ray.radiance = Color(1, 1, 1);
@@ -76,12 +120,16 @@ Ray build_path(Scene myscene, Ray& origin_ray) {
 		else {
 			// TEMP: reflect as if a perfect mirror and add to the ray path
 			// THIS IS WRONG AND SHOULD BE REPLACED WITH A MATERIAL-SPECIFIC SCATTER FUNCTION
-			auto t = rec.t;
-			Ray ref_ray(origin_ray.at(t), unit_vector(origin_ray.get_direction() - 2 * dot(origin_ray.get_direction(), rec.normal) * rec.normal));
-			origin_ray.next_ray = &ref_ray;
-			ref_ray.prev_ray = &origin_ray;
-			ref_ray.depth = origin_ray.depth + 1;
-			return build_path(myscene, ref_ray);
+			auto t = nearest_rec.t;
+			//std::cout << "\nOrigin of the reflected ray: ";
+			//std::cout << origin_ray.at(t);
+			ref_ray = new Ray(origin_ray.at(t), unit_vector(origin_ray.get_direction() - 2 * dot(origin_ray.get_direction(), nearest_rec.normal) * nearest_rec.normal));
+			origin_ray.next_ray = ref_ray;
+			ref_ray->prev_ray = &origin_ray;
+			ref_ray->depth = origin_ray.depth + 1;
+			//std::cout << "\nConnected ray of depth " << origin_ray.depth << " to ray of depth " << ref_ray->depth;
+			//std::cout << "\nOrigin ray now connects to ray of depth " << origin_ray.next_ray->depth;
+			return build_path(myscene, *ref_ray);
 		}
 	}
 	else {
@@ -91,7 +139,7 @@ Ray build_path(Scene myscene, Ray& origin_ray) {
 	}
 }
 
-Color terminate_ray(Scene myscene, Ray r) {
+Color terminate_ray(Scene myscene, Ray& r) {
 	// If the ray terminates on a light -- the radiance is (1, 1, 1)
 	// If the ray terminates on a lambertian surface, the radiance is the brightness of the surface after direct light calculation
 
@@ -100,7 +148,7 @@ Color terminate_ray(Scene myscene, Ray r) {
 		return r.radiance;
 	}
 	else {
-		std::cout << r;
+		//std::cout << "\nTerminating " << r;
 		return terminate_ray(myscene, *r.prev_ray);
 	}	
 }
--- Tracer2/Tracer.h
+++ Tracer2/Tracer.h
@@ -15,7 +15,11 @@ Color trace_ray(Scene myscene, const Ray& r, int depth);
 // The path tracer starts by building a path, then goes through the path in reverse and returns a color
 Color path_tracer(Scene myscene, Ray& r);
 
+void print_raypath(Ray& first_ray);
+
+void print_reverse_raypath(Ray& last_ray);
+
 // Takes a ray originating from the camera viewport, builds a ray path and then returns the final ray (termination)
 Ray build_path(Scene myscene, Ray& origin_ray);
 
-Color terminate_ray(Scene myscene, Ray r);
+Color terminate_ray(Scene myscene, Ray& r);
