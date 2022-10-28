c07101fdff6b647dd694b9f817c93f3029c222f7
Max Eriksson <maxer142@student.liu.se>
2022-10-28 16:03:34 +0200

    Glömde pusha en fil.

--- Tracer2/Tracer.h
+++ Tracer2/Tracer.h
@@ -13,7 +13,7 @@ class Ray;
 Color trace_ray(Scene myscene, const Ray& r, int depth);
 
 // Return the amount of direct light hitting a surface
-double direct_light(Scene& myscene, int shadow_rays, Point3D intersection_point, Vec3 object_normal);
+Color direct_light(Scene& myscene, int shadow_rays, Point3D intersection_point, Vec3 object_normal, Color object_color);
 
 // The path tracer starts by building a path, then goes through the path in reverse and returns a color
 Color path_tracer(Scene myscene, Ray& r);
