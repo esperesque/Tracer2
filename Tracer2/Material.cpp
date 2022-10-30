e8ffc451d724cc64542b1f87ab4fa11f20bdd749
Max Eriksson <maxer142@student.liu.se>
2022-10-30 14:43:33 +0100

    Russian Roulette implementerat
    Implementerade Russian Roulette. Ligger som en del i getRandomDirection-funktionen.

--- Tracer2/Material.cpp
+++ Tracer2/Material.cpp
@@ -29,7 +29,6 @@ Ray* Material::reflect_ray(Scene& myscene, const Ray& r, hit_record& rec, int de
 		ref_ray = new Ray(r.at(rec.t), unit_vector(r.get_direction() - 2 * dot(r.get_direction(), rec.normal) * rec.normal));
 	}
 	else {
-		Vec3 new_normal = unit_vector(rec.normal + 0.5*Vec3::random_in_unit_sphere());
 		ref_ray = getRandomDirection(r, rec);
 	}
 
@@ -38,30 +37,40 @@ Ray* Material::reflect_ray(Scene& myscene, const Ray& r, hit_record& rec, int de
 }
 
 Ray* Material::getRandomDirection(const Ray& r, hit_record& rec){
-	double p = 0.25;
-	double yi = random_double();
-	double yi_next = random_double();
-	double azimuth = 2 * M_PI * yi / p;
-	double inclination = acos(sqrt(yi_next));
+	double p = random_double();
+	Ray* rayOut = new Ray();
+	int n = 1;
 
-	//Conversion of hemispherical into cartesian
-	double x0 = cos(azimuth) * sin(inclination);
-	double y0 = sin(azimuth) * sin(inclination);
-	double z0 = cos(inclination);
+	for (int i = 0; i < n; i++) {
+		double yi = random_double();
+		double yi_next = random_double();
+		double azimuth = 2 * M_PI * yi / p;
+		double inclination = acos(sqrt(yi_next));
 
-	//Certesian local system to world coordinate system
-	Vec3 N = rec.normal;
-	Vec3 X = N * (dot(N, r.get_direction())) - unit_vector(r.get_direction());
-	Vec3 Z = N;
-	Vec3 Y = cross(Z, X);
+		//Russian roulette
+		if (azimuth > 2 * M_PI && r.depth > 1) {
+			n = i + 1;
+			break;
+		}
 
-	//Calculations
-	double x = x0 * X.get_x() + y0 * Y.get_x() + z0 * Z.get_x();
-	double y = x0 * X.get_y() + y0 * Y.get_y() + z0 * Z.get_y();
-	double z = x0 * X.get_z() + y0 * Y.get_z() + z0 * Z.get_z();
+		//Conversion of hemispherical into cartesian
+		double x0 = cos(azimuth) * sin(inclination);
+		double y0 = sin(azimuth) * sin(inclination);
+		double z0 = cos(inclination);
 
-	Vec3 directionRay = { x,y,z };
-	Ray* rayOut = new Ray(r.at(rec.t), directionRay);
+		//Certesian local system to world coordinate system
+		Vec3 N = rec.normal;
+		Vec3 X = N * (dot(N, r.get_direction())) - unit_vector(r.get_direction());
+		Vec3 Z = N;
+		Vec3 Y = cross(Z, X);
 
+		//Calculations
+		double x = x0 * X.get_x() + y0 * Y.get_x() + z0 * Z.get_x();
+		double y = x0 * X.get_y() + y0 * Y.get_y() + z0 * Z.get_y();
+		double z = x0 * X.get_z() + y0 * Y.get_z() + z0 * Z.get_z();
+
+		Vec3 directionRay = { x,y,z };
+		rayOut = new Ray(r.at(rec.t), directionRay);
+	}
 	return rayOut;
 }
